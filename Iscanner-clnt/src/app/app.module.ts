8b9fb36dbcddae8a3d6b26b7b28f33d88dfec2e3
toniihrd <toniihrd@gmail.com>
2020-07-07 16:25:13 +0100

    home page

--- Iscanner-clnt/src/app/app.module.ts
+++ Iscanner-clnt/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 @NgModule({
   declarations: [
     AppComponent,
+    QuestionnaireComponent,
   ],
   imports: [
     BrowserModule,
